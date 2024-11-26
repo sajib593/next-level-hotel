ddb86ae860da6e5d6efdeebb4f2a0c9ef9890e95
sajib593 <sajib.hoossain11@gmail.com>
2024-11-26 23:42:43 +0600

    first commit

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+
+
+const page = () => {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold text-center mt-10">Dashboard</h1>
+        </div>
+    );
+};
+
+export default page;
