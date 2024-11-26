1e55985be1c5f29c939a37c4895c2f5c879eb679
sajib593 <sajib.hoossain11@gmail.com>
2024-11-26 23:57:13 +0600

    third commit

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -4,6 +4,11 @@ const page = () => {
     return (
         <div>
             <h1 className="text-2xl font-bold text-center mt-10">This is Dashboard</h1>
+
+
+            <h2 className="text-xl font-bold text-center mt-10">i make a checkout</h2>
+
+            
         </div>
     );
 };
