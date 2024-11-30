55b6605a0e54ca7159d33b674f2d0ac13580a513
sajib593 <sajib.hoossain11@gmail.com>
2024-11-30 20:51:03 +0600

    navbar update

--- app/hotels/[id]/page.js
+++ app/hotels/[id]/page.js
@@ -9,18 +9,22 @@ const HotelDetailPage = async ({ params }) => {
     const { name, location, rating, pricePerNight, imageUrl } = details
     return (
         <div className="p-6">
-            <h1 className="text-2xl font-bold">{name}</h1>
-            {imageUrl && (
+            <h1 className="text-5xl font-bold text-center">{name}</h1>
+            <br></br>
+         <div className='flex justify-center'>
+         {imageUrl && (
                 <Image
                     src={imageUrl} // Use the correct image URL
                     alt={name}
-                    width={400}
-                    height={256}
+                    width={800}
+                    height={200}
                     className="rounded-md"
                 />
             )}
-            <p className="text-gray-600">Location: {location}</p>
-            <p className="text-gray-800 font-bold mt-2">Price: ${pricePerNight}</p>
+         </div>
+            <p className="text-gray-600 text-3xl text-center">Location: {location}</p>
+            <p className="text-gray-800 text-xl font-bold mt-2 text-center">Price: ${pricePerNight}</p>
+            <p className="text-gray-800 text-xl font-bold mt-2 text-center">Rating: {rating}</p>
         </div>
     );
 };
