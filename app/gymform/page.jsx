53bfc7ed1e284c0216bbb7a762fd33df601b8a28
Rayhan9966 <rayhancse162gmail.com>
2024-12-01 16:37:51 +0400

    added gym card and form

---
+++ app/gymform/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const gymformpage = () => {
+    return (
+        <div>
+            <h1>this is form page</h1>
+        </div>
+    );
+};
+
+export default gymformpage ;
