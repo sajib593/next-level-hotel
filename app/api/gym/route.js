e7968af6e94b1d892f2762d438b2a2d1c6cdd2d6
Rayhan9966 <rayhancse162gmail.com>
2024-12-07 18:38:30 +0400

    gym plan connect to mpngodb

---
+++ app/api/gym/route.js
@@ -0,0 +1,15 @@
+
+
+import { connectDB } from "@/lib/connectDB";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+    const db = await connectDB();
+    const gymCollection = db.collection('gymplan')
+    try {
+        const gymPlan = await gymCollection.find().toArray();
+        return new NextResponse(JSON.stringify(gymPlan), { status: 200 });
+    } catch (error) {
+        console.log(error)
+    }
+};
